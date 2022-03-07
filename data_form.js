7222e16aaa668873d1f6512e92db26942f79b987
Camilo Huertas <83623661+juankamilohuertas@users.noreply.github.com>
2022-03-06 22:41:39 -0500

    empezando con la entrada del codigo de barras

---
+++ data_form.js
@@ -0,0 +1,10 @@
+const codigo = document.getElementById("codigo");
+const tabla_data_code = document.getElementById("tabla_data_code ");
+console.log(localStorage.length)
+const ingresoCodigo = ()=>{
+
+}
+/* const validandoIngreso = (res)=>{
+   
+} */
+codigo.addEventListener("change", ingresoCodigo);
