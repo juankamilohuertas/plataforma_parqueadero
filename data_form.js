4b40d044916da66898ac25fb7b7efd9ed465b9d5
Camilo Huertas <83623661+juankamilohuertas@users.noreply.github.com>
2022-03-07 19:54:23 -0500

    datos del usuario

--- data_form.js
+++ data_form.js
@@ -1,10 +1,56 @@
 const codigo = document.getElementById("codigo");
-const tabla_data_code = document.getElementById("tabla_data_code ");
-console.log(localStorage.length)
+const tablaDataUsuarios = document.getElementById("datos_usuario");
+const tablaDataVehiculos = document.getElementById("datos_vehiculo");
+/* convirtiendo los datos del local storage en un array */
+let dataTodosUsers = localStorage.length+1;
 const ingresoCodigo = ()=>{
-
+    let val;
+    let arr =[];
+    for(let index = 1; index < dataTodosUsers;index++) {
+       val = localStorage.getItem("KEY_"+index);
+       arr.push(val)
+    }   
+   recibiendoDatos(arr);
 }
-/* const validandoIngreso = (res)=>{
-   
-} */
 codigo.addEventListener("change", ingresoCodigo);
+/* recibiendo el array y filtrando la info para buscar el codigo de barras */
+let recibiendoDatos =(arr)=>{
+    let dataCode = [];
+    for (const key in arr) {
+        let ultimoArr = arr[key].split(",");
+        dataCode.push(ultimoArr);
+    }
+    mostrandoInfo(dataCode);
+}
+/* mostrando la info recibida por los datos filtrados */
+const mostrandoInfo = (dataCode)=>{
+    let res = dataCode.filter(i => i.includes(codigo.value));
+    if(res[0] != undefined){
+        if(res[0][3] == codigo.value){
+            let users;
+            if(res[0][1] != "MOTO" && res[0][1] != "VEHICULO"){
+                users = `
+                <tr>
+                <td>fecha</td>
+                <td>hoara</td>
+                <td>${res[0][0]}</td>
+                <td>${res[0][1]} ${res[0][2]}</td>
+                <td>${res[0][3]}</td>
+                </tr>`;
+                tablaDataUsuarios.innerHTML += users;   
+            }else{
+                users = `
+                <tr>         
+                <td>${res[0][0]}</td>
+                <td>${res[0][1]}</td>
+                <td>${res[0][2]}</td>
+                <td>${res[0][3]}</td>
+                </tr>`;
+                tablaDataVehiculos.innerHTML += users;   
+            }
+        }
+    }else{
+        alert("no esta registrado/a");
+    }
+    
+}
