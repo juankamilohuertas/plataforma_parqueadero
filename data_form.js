f98860475076c8ce588f6628856cf624c92bc592
Camilo Huertas <83623661+juankamilohuertas@users.noreply.github.com>
2022-03-10 00:21:18 -0500

    casi terminado

--- data_form.js
+++ data_form.js
@@ -61,21 +61,30 @@ const mostrandoInfo = (dataCode)=>{
     } 
     codigo.value= "";
 }
-/* asignando cada dato a la tabla y si no se repite el valor re incerta en el dom */
-const infoDataUsers = (res,users)=>{
+/* asignando cada dato a la tabla y si no se repite el valor re incerta en el dom y llevando la data al sessionstorage*/
+let registroDeTabla = sessionStorage.length;
+const infoDataUsers = (res)=>{
       if(arr.includes(codigo.value)){
         tablaDataUsuarios.innerHTML += ""; 
       }else{
+        let registro = [fecha(),hora(),res[0][0],res[0][1]+" "+res[0][2],res[0][3]]
+        sessionStorage.setItem("TABLA_"+registroDeTabla++, registro);
+        let arr = [];
+        for (let i = 0; i < registroDeTabla; i++) {
+            arr.push(sessionStorage.getItem("TABLA_"+i))
+        }
+        let valor = arr;
+        console.log(valor)
         users = `
         <tr>
-        <td>${fecha()}</td>
-        <td>${hora()}</td>
-        <td>${res[0][0]}</td>
-        <td>${res[0][1]} ${res[0][2]}</td>
-        <td>${res[0][3]}</td>
+        <td>${valor[arr.length-1].split(",")[0]}</td>
+        <td>${valor[arr.length-1].split(",")[1]}</td>
+        <td>${valor[arr.length-1].split(",")[2]}</td>
+        <td>${valor[arr.length-1].split(",")[3]}</td>
+        <td>${valor[arr.length-1].split(",")[4]}</td>
         </tr>`;
-        tablaDataUsuarios.innerHTML += users;  
-      }
+        tablaDataUsuarios.innerHTML += users; 
+    }
 }
 const infoDataVehiculos = (res,vehiculos)=>{
     if(arr.includes(codigo.value)){
