4e3849784d1a624f2c85d50d92e6719bdbbceb51
Camilo Huertas <83623661+juankamilohuertas@users.noreply.github.com>
2022-05-01 01:16:52 -0500

    actualizando

--- btn_eliminar.js
+++ btn_eliminar.js
@@ -2,10 +2,10 @@ const btn_eliminar_todo = document.getElementById("btn_eliminar_todo");
 const btn_eliminar_todo1 = document.getElementById("btn_eliminar_todo1");
 const msj_eliminar_tabla = document.getElementById("msj_time_eliminar_data");
 let btnEliminarFila = document.querySelectorAll(".btn_eliminar");
-/* mensaje de tiempo que le queda por descargar la tabla */
-msj_eliminar_tabla.innerHTML = `Mañana a las <b>23:00 p.m</b> se borra la tabla que creaste hoy, ¡--DESCARGALA--!`;
-if(tablaDataUsuarios.children.length != 0 || tablaDataVehiculos.children.length != 0){
-    // eliminando todas las tablas #0 dando click en el Boton ELIMINAR TODO
+if(primerTurnoTablaUsers.children.length != 0 || primerTurnoTablaVeh.children.length != 0 ||
+    segundoTurnoTablaUsers.children.length != 0 || segundoTurnoTablaVeh.children.length != 0 ||
+    tercerTurnoTablaUsers.children.length != 0 || tercerTurnoTablaVeh.children.length != 0){
+        // eliminando todas las tablas #0 dando click en el Boton ELIMINAR TODO
     btn_eliminar_todo.addEventListener("click", ()=>{
         let tablas = document.querySelectorAll(".tablaUser");
         for (let i = 0; i < tablas.length; i++) {
@@ -13,19 +13,34 @@ if(tablaDataUsuarios.children.length != 0 || tablaDataVehiculos.children.length 
         setTimeout(()=>{
             const preguntarBorar = confirm("Ya descargaste esta tabla () veces deseas Borarla");
             if(preguntarBorar){
-            localStorage.removeItem("USUARIO_");
-            localStorage.removeItem("VEHICULOS_");
+            localStorage.removeItem("PRIMER_TURNO_U_");
+            localStorage.removeItem("SEGUNDO_TURNO_U_");
+            localStorage.removeItem("TERCER_TURNO_U_");
+            localStorage.removeItem("PRIMER_TURNO_V_");
+            localStorage.removeItem("SEGUNDO_TURNO_V_");
+            localStorage.removeItem("TERCER_TURNO_V_");
             localStorage.removeItem("INGRESO");
             localStorage.setItem("FECHA", fechaActual);
             localStorage.setItem("CONTEO", 0);
+            localStorage.setItem("CONTEO_1", 0);
+            localStorage.setItem("CONTEO_2", 0);
             localStorage.setItem("INGRESO_NOVEDAD", JSON.stringify(["S/N"]));
+            localStorage.setItem("INGRESO_NOVEDAD_1", JSON.stringify(["S/N"]));
+            localStorage.setItem("INGRESO_NOVEDAD_2", JSON.stringify(["S/N"]));
+            localStorage.setItem("CANTIDAD_DE_VEHICULOS", 0);
+            localStorage.setItem("CANTIDAD_DE_VEHICULOS_1", 0);
+            localStorage.setItem("CANTIDAD_DE_VEHICULOS_2", 0);
+            localStorage.setItem("TABLA_2_ACTIVA", true);
             }
             codigo.focus();
             window.location.reload();
         },100)
     })
 }
-if(tablaDataUsuarios1.children.length != 0 || tablaDataVehiculos1.children.length != 0){
+
+if(primerTurnoTablaUsers1.children.length != 0 || primerTurnoTablaVeh1.children.length != 0 ||
+    segundoTurnoTablaUsers1.children.length != 0 || segundoTurnoTablaVeh1.children.length != 0 ||
+    tercerTurnoTablaUsers1.children.length != 0 || tercerTurnoTablaVeh1.children.length != 0){
     // eliminando todas las tablas #1 dando click en el Boton ELIMINAR TODO
     btn_eliminar_todo1.addEventListener("click", ()=>{
         let tablas1 = document.querySelectorAll(".tablaUser1");
@@ -34,9 +49,23 @@ if(tablaDataUsuarios1.children.length != 0 || tablaDataVehiculos1.children.lengt
         setTimeout(()=>{
             const preguntarBorar = confirm("Ya descargaste esta tabla () veces deseas Borarla");
             if(preguntarBorar){
-            localStorage.removeItem("USUARIO_1");
-            localStorage.removeItem("VEHICULOS_1");
-            localStorage.setItem("FECHA", fechaActual);
+            localStorage.removeItem("PRIMER_TURNO_U_1");
+            localStorage.removeItem("SEGUNDO_TURNO_U_1");
+            localStorage.removeItem("TERCER_TURNO_U_1");
+            localStorage.removeItem("PRIMER_TURNO_V_1");
+            localStorage.removeItem("SEGUNDO_TURNO_V_1");
+            localStorage.removeItem("TERCER_TURNO_V_1");
+            localStorage.removeItem("INGRESO");
+            localStorage.setItem("CONTEO_", 0);
+            localStorage.setItem("CONTEO_1_1", 0);
+            localStorage.setItem("CONTEO_2_1", 0);
+            localStorage.setItem("INGRESO_NOVEDAD_", JSON.stringify(["S/N"]));
+            localStorage.setItem("INGRESO_NOVEDAD_1_1", JSON.stringify(["S/N"]));
+            localStorage.setItem("INGRESO_NOVEDAD_2_2", JSON.stringify(["S/N"]));
+            localStorage.setItem("CANTIDAD_DE_VEHICULOS_", 0);
+            localStorage.setItem("CANTIDAD_DE_VEHICULOS_1_1", 0);
+            localStorage.setItem("CANTIDAD_DE_VEHICULOS_2_1", 0); 
+            localStorage.setItem("TABLA_2_ACTIVA", false);
             }
             window.location.reload();
             codigo.focus();
@@ -44,6 +73,24 @@ if(tablaDataUsuarios1.children.length != 0 || tablaDataVehiculos1.children.lengt
     })
 }
 
+
+/* +++++++++++++++++++++  PENDIENTE +++++++++++++++++++++++ */
+
+
+
+/* activando el botton de eliminar */
+localStorage.setItem("ACTIVAR_BORRAR_TABLA_1", false);
+localStorage.setItem("ACTIVAR_BORRAR_TABLA_2", false);
+
+
+
+
+/* activando la tabla 2 para usar */
+let tablaActiva_ = localStorage.getItem("TABLA_2_ACTIVA");
+if(tablaActiva_ == "true"){
+    tablaActiva.checked=tablaActiva_;
+}
+   
 let btnEliminar = document.body.getElementsByTagName("button");
 for (let i = 0; i < btnEliminar.length; i++) {
     btnEliminar[i].addEventListener("click", ()=>{
@@ -51,22 +98,47 @@ for (let i = 0; i < btnEliminar.length; i++) {
     }) 
 } 
 codigo.focus();
-
-
-
-
 /* borrando toda la data despues de dos dias*/
-
 const dias = new Date();
 let fechaActual = dias.getDate();
-let trayendoFecha = localStorage.getItem("FECHA");
-if(trayendoFecha < fechaActual){
-msj_eliminar_tabla.innerHTML = `Recuerda que hoy se borra a las <b>23:00 p.m</b> la tabla que creaste ayer, ¡--DESCARGALA--!`;
-if(dias.getHours() == 23 ){
-    localStorage.removeItem("USUARIO_");
-    localStorage.removeItem("VEHICULOS_");
-    localStorage.removeItem("FECHA");
-    localStorage.setItem("FECHA", fechaActual);}
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* desabilitando btn de tabla#1 para no borar */
+if(dias.getHours() > 23 || dias.getHours() < 8){
+    btn_eliminar_todo.setAttribute("disabled","true");
+    btn_eliminar_todo.title="Puedes Borar la Tabla a Partir de las 8:00 a.m";
+    btn_eliminar_todo1.setAttribute("disabled","true");
+    btn_eliminar_todo1.title="Puedes Borar la Tabla a Partir de las 8:00 a.m";
+}else{
+    btn_eliminar_todo.title="";
 }
 
+
+
+
+let trayendoFecha = localStorage.getItem("FECHA");
+if(trayendoFecha < fechaActual || trayendoFecha > fechaActual){
+    msj_eliminar_tabla.innerHTML = `Recuerda que hoy se borra a las <b>12:00 a.m</b> la tabla que creaste ayer, ¡--DESCARGALA--!`;
+    if(dias.getHours() == 0){
+        localStorage.removeItem("PRIMER_TURNO_U_");
+        localStorage.removeItem("SEGUNDO_TURNO_U_");
+        localStorage.removeItem("TERCER_TURNO_U_");
+        localStorage.removeItem("PRIMER_TURNO_V_");
+        localStorage.removeItem("SEGUNDO_TURNO_V_");
+        localStorage.removeItem("TERCER_TURNO_V_");
+        localStorage.removeItem("INGRESO");
+        localStorage.setItem("FECHA", fechaActual);
+        localStorage.setItem("CONTEO", 0);
+        localStorage.setItem("CONTEO_1", 0);
+        localStorage.setItem("CONTEO_2", 0);
+        localStorage.setItem("INGRESO_NOVEDAD", JSON.stringify(["S/N"]));
+        localStorage.setItem("INGRESO_NOVEDAD_1", JSON.stringify(["S/N"]));
+        localStorage.setItem("INGRESO_NOVEDAD_2", JSON.stringify(["S/N"]));
+        localStorage.setItem("CANTIDAD_DE_VEHICULOS", 0);
+        localStorage.setItem("CANTIDAD_DE_VEHICULOS_1", 0);
+        localStorage.setItem("CANTIDAD_DE_VEHICULOS_2", 0);
+        localStorage.setItem("TABLA_2_ACTIVA", true);
+    }
+}else{
+    msj_eliminar_tabla.innerHTML = `Mañana las <b>23:00 p.m</b> se borra la tabla que creaste hoy, ¡--DESCARGALA--!`;
+}
 /* CREAR NUEVA TABLA */
